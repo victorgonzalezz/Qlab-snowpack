ae519d3480b1790c192506dda330de477fe1777c
Victor <vitopcg@gmail.com>
2022-04-01 14:02:45 +0100

    commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>Hi</div>, document.getElementById("root"));
+
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
